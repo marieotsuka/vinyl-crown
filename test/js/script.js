ff1f4f47e2d7dbe4f89f4fd24d796c596b1dcf41
Marie Otsuka <motsuka.gd@gmail.com>
2021-07-05 11:26:29 -0400

    add spotify api integration and tracklist

--- test/js/script.js
+++ test/js/script.js
@@ -7,5 +7,33 @@ $(document).ready(function(){
 	});
 
 
+	var spotifyApi = new SpotifyWebApi();
+	spotifyApi.setAccessToken('BQA7AZvUJv1V4fyOwqLABPjTFK3401l3Nbnl4lLd2w7AnlouU4Ahhp8pGdWYJOy0QO6ruEQrUEzVaz8EfcYCpQr-0RKkqkskW4CxKqh2WG3MGNh3M3F8efbezqI6sq5O_ouW1_Pw1bA');
+
+	
+	spotifyApi.getAlbum('33YUhFYzDdg3PmQSutGXkZ', function (err, data) {
+	  if (err) {
+	  	console.error(err);
+	  }else {
+	  	console.log('Album Data', data);
+	  	let tracks = data.tracks.items;
+	  	tracks.forEach( t => {
+	  		$('#tracks').append(`<div class="track" data-preview="${t.preview_url}">${t.name}</div>`);
+	  	});
+
+
+	  		$('.track').on('click', function(){
+	  			console.log('play');
+	  			let preview = $(this).data('preview');
+	  			let audioObject = new Audio(preview);
+	  	        audioObject.play();
+	  	        $(this).addClass('playing');
+	  		});
+	  }
+	});
+
+
+
+
 });
 
